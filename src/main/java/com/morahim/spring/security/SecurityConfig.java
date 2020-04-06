8890b35c734f1377ade3d0071202aae6702f8b4e
Mo <rahim.muhammad@pm.me>
2020-04-06 21:19:12 +0100

    service discovery

--- src/main/java/com/morahim/spring/security/SecurityConfig.java
+++ src/main/java/com/morahim/spring/security/SecurityConfig.java
@@ -1,29 +1,26 @@
 package com.morahim.spring.security;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
-@EnableWebFluxSecurity
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     @Lazy
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(@Qualifier("inMemoryUserDetailsManager") UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder encoder() {
@@ -31,18 +28,25 @@ public class SecurityConfig {
     }
 
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange()
-                .pathMatchers("/design", "/orders").hasAuthority("USER")
-                .anyExchange().permitAll()
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService)
+                .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/design", true)
                 .and()
                 .logout()
-                .and().csrf().disable()
-                .build();
+                .logoutSuccessUrl("/")
+                .and().csrf().disable();
     }
 }
